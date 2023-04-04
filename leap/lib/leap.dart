da5c35bd8f3d02dde121f2b8e65c896fb417ae90
SoojinKang0127 <iamhajaml@naver.com>
2023-04-04 22:13:09 +0900

    Exercise 3

---
+++ leap/lib/leap.dart
@@ -0,0 +1,17 @@
+class Leap {
+  bool leapYear(int year) {
+    if (year % 4 != 0) {
+      return false;
+    }
+
+    if (year % 100 != 0) {
+      return false;
+    }
+
+    if (year % 400 != 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
